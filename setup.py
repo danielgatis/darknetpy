4f8e2776645227e52d0758cc69d0fc3ab1e3625e
Daniel Gatis <danielgatis@gmail.com>
2017-09-01 18:44:26 -0300

    README

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ modules = [
 
 setup(
     name='darknetpy',
-    version='1.0',
+    version='1.2',
     author='Daniel Gatis Carrazzoni',
     author_email='danielgatis@gmail.com',
     url='https://github.com/danielgatis/darknetpy',
